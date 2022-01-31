637629d88798f235ada08d018fd3830e5e91265f
siposadam <siposadam94@gmail.com>
2022-01-31 21:38:41 +0100

    login/logout function

--- warehouse/web/src/main/java/hu/siposadam/servlet/LoginServlet.java
+++ warehouse/web/src/main/java/hu/siposadam/servlet/LoginServlet.java
@@ -10,13 +10,15 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
+        String name = request.getParameter("username");
         String password = request.getParameter("password");
 
-        if ("admin".equals(name) && "pw".equals(password)) {
+        if ("admin".equals(name) && "password".equals(password)) {
             request.getSession().setAttribute("authenticated", Boolean.TRUE);
 
             response.sendRedirect("secured/profile.html");
+            return;
         }
+        response.sendRedirect("login.html");
     }
 }
